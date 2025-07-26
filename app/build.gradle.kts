b9addf357114d591f966b1d89ecb634de0e7a7fe
Nina Semkina <nintv00@gmail.com>
2025-07-26 10:33:18 +0700

    update dependencies and configuration for Gradle and Android SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 android {
     namespace = "com.rumpilstilstkin.gloomhavenhelper"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.rumpilstilstkin.gloomhavenhalper"
