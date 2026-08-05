506ca8e9ef494b48657d68a317c0fb3e75f19cdb
Nina Semkina <nina.semkina@b2broker.com>
2026-08-05 14:47:11 +0700

    up version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     defaultConfig {
         applicationId = "com.rumpilstilstkin.gloommaster"
         minSdk = 29
-        versionCode = 14
-        versionName = "2.0.4"
+        versionCode = 15
+        versionName = "2.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
